1886340a3eb1f0b5e014382853802fa0df26d943
Noah Teshu <nteshu@gmail.com>
2015-11-30 18:15:18 -0500

    add list view for expansion of service categories

--- HelpSteps/www/js/app.js
+++ HelpSteps/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic',  'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,13 +20,17 @@ angular.module('starter', ['ionic'])
 
 .config(function($stateProvider, $urlRouterProvider){
   $stateProvider
-    .state('launch', {
-      url: '/launch',
-      views: {
-        'launch': {
-          templateUrl: 'templates/launch.html'
-        }
-      }
-    })
+  .state('launch', {
+    url: '/launch',            
+    templateUrl: 'templates/launch.html'
+        
+  })
+
+  .state('categoryList', {
+    url: '/categoryList',
+    templateUrl: 'templates/categoryList.html'
+    
+  })
+
   $urlRouterProvider.otherwise('/launch');
-})
+});
